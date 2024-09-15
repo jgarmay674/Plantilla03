2ae60829dc4f23dc93384d1ea591b10ed9846208
jgarmay674 <jgarmay674@g.educaand.es>
2024-09-15 10:01:57 +0200

    Añadiendo la Plantilla03

---
+++ ejercicios/ejer02/js/script.js
@@ -0,0 +1,6 @@
+"use strict"
+
+const boton = document.getElementById("boton");
+const resultado = document.getElementById("resultado");
+
+boton.addEventListener("click", mostrar);
---
+++ ejercicios/js/funciones.js
@@ -0,0 +1,6 @@
+"use strict"
+
+function mostrar() {
+    console.log(`Estoy mostrando el resultado del ${resultado.getAttribute('name')}`);
+    resultado.innerHTML = `Estoy mostrando el resultado del ${resultado.getAttribute('name')}`;
+}
